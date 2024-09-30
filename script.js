a5fef81aadd0c88b9786435f8377c348592b052f
EnderDeV123 <152074704+EnderDeV123@users.noreply.github.com>
2024-09-30 08:45:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true;
+let turnO = true; // Player's turn (O) or AI's turn (X)
 
 const winPatterns = [
     [0, 1, 2],
@@ -15,62 +15,78 @@ const winPatterns = [
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8],
-]
+];
 
 const resetGame = () => {
-    turnO = true;
+    turnO = true; // Reset to player's turn
     enableBoxes();
     msgContainer.classList.add("hide");
 }
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if(turnO === true){
-            box.innerText = "O";
-            turnO = false;
-        }else{
-            box.innerText = "x";
-            turnO = true;
+        if (turnO) {
+            box.innerText = "O"; // Player makes a move
+            box.disabled = true;
+            if (!checkWinner()) {
+                turnO = false; // Switch turn to AI
+                setTimeout(aiTurn, 1000); // Delay AI's turn by 1 second
+            }
         }
-        box.disabled = true;
+    });
+});
 
+const aiTurn = () => {
+    const availableBoxes = [...boxes].filter(box => box.innerText === "");
+    if (availableBoxes.length > 0) {
+        const randomBox = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+        randomBox.innerText = "X"; // AI makes a move
+        randomBox.disabled = true;
         checkWinner();
-    })
-})
+        turnO = true; // Switch turn back to player
+    }
+}
 
 const enableBoxes = () => {
-    for (let box of boxes){
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
 }
 
 const disableBoxes = () => {
-    for (let box of boxes){
+    for (let box of boxes) {
         box.disabled = true;
     }
 }
 
-
-const ShowWinner = (winner) => {
-    msg.innerText =`Congratulations , Winner is ${winner}`;
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
-    
 }
 
 const checkWinner = () => {
-    for(let pattern of winPatterns){
+    for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                ShowWinner(pos1Val);
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+                return true; // Winner found
             }
         }
     }
+    // Check for a draw
+    if ([...boxes].every(box => box.innerText !== "")) {
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+        return true;
+    }
+    return false; // No winner
 };
 
 newGameBtn.addEventListener("click", resetGame);
